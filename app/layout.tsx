2ee55b6738e4231e069478ed7308578d0faafaf2
Plaetorius <plaetorius@gmail.com>
2025-08-24 13:06:26 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Prediction.live",
+  description: "Don't watch e-sport. Predict it.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
