6e859f069c6f335b26178b2025506cb7f4c26213
Plaetorius <plaetorius@gmail.com>
2025-10-26 16:58:25 +0100

    New mojo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Prediction.Live",
-  description: "Don't watch e-sport. Predict it.",
+  description: "Live esports. Predict it.",
 };
 
 export default function RootLayout({
